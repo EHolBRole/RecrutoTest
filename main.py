37c135666fad95c086ebb382cf1711e0ede8c19a
EHolBRole <ekholo.banderole@mail.ru>
2025-01-16 15:21:01 +0300

    Docker-compose fix

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def greet():
+    name = request.args.get('name', 'Recruto')
+    message = request.args.get('message', 'Давай дружить!')
+    return f"Hello {name}! {message}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
